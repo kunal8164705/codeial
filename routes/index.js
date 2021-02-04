62b1a4a09af1fb159738ab1ca79972ffa627375d
kunal8164705 <kunal8164705@gmail.com>
2021-02-04 17:37:29 +0530

    add controller for users and router for users home

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ const homeControllers=require('../controllers/home_controller');
 console.log('router loaded');
 
 router.get('/',homeControllers.name);
+router.use('/users',require('./users'));
 // router.get('/', homeControllers.home);
 module.exports=router;
