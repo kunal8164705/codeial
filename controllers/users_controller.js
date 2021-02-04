5b658f1ee6ccfb2c75a86a8dfc02bdccca3db354
kunal8164705 <kunal8164705@gmail.com>
2021-02-04 18:09:27 +0530

    add controller for users post and router for users post

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile=function(req,res){
     return res.end('<h1>user profile</h1>');
 }
+
+module.exports.post=function(req,res){
+    return res.end('<h1> user post</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const router=express.Router();
 const homeControllers=require('../controllers/home_controller');
 
 console.log('router loaded');
-
-router.get('/',homeControllers.name);
+router.get('/', homeControllers.home);
+router.get('/name',homeControllers.name);
 router.use('/users',require('./users'));
-// router.get('/', homeControllers.home);
+
 module.exports=router;
