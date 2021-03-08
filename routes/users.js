5898bcec468ebaa9bbb1a8ec65f9c67f98078b8c
kunal8164705 <kunal8164705@gmail.com>
2021-03-08 20:00:07 +0530

    successfully added update profile action

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const passport=require('passport');
 const userController=require('../controllers/users_controller');
 
 router.get('/profile/:id',passport.checkAuthentication,userController.profile);
+router.post('/update/:id',passport.checkAuthentication,userController.update);
 router.get('/post',userController.post);
 
 
