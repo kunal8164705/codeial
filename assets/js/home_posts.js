f5f1bb48c47aecf3f989224358b2fbbe02304b51
kunal8164705 <kunal8164705@gmail.com>
2021-04-21 18:15:37 +0530

    converting data to xml while creating post

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -10,7 +10,8 @@
                 url:'/posts/create',
                 data:newPostForm.serialize(),
                 success:function(data){
-                    console.log(data);
+                    let newPost=newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
                 },
                 error:function(error){
                     console.log(error.responseText);
@@ -22,6 +23,46 @@
     }
 
     //method to create a post in DOM
+    let newPostDom=function(post){
+        return $(`<li id="post-${ post._id }">
+        <p>
+            
+            <small>
+                <a  class="delete-post-button" href="/posts/destroy/ ${post.id }">Delete post</a>
+            </small>
+            
+        <small>
+            ${post.user.name}
+            <br>
+             ${post.content}
+            
+        </small>
+        </p>
+        <div class="post-comments">
+            
+                <form action="comments/create" method="post">
+                <input type="text" name="content" placeholder="Type here to add comment" required>
+                <input type="hidden" name="post" value="${post._id }">
+                <input type="submit" value="Add Comment">
+    
+                </form>
+                
+               
+    
+                <div class="post-comments-list">
+                    <ul id="post-comments-${post._id}">
+                            
+                            
+                    </ul>
+    
+                </div>
+        </div>
+    
+    </li>`)
+    }
+
+
+
 
     createPost();
    
